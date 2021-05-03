42bd8a02aedc3094471183ae5b4e3fc61343f812
David Doty <doty@ucdavis.edu>
2021-05-03 12:50:31 -0700

    removed tests/util.py to make importing easier in GitHub CI

--- tests/ppsim_tests.py
+++ tests/ppsim_tests.py
@@ -1,7 +1,8 @@
 import unittest
+from typing import List, Dict, Any
 
+import numpy
 from ppsim import species, reactions_to_dict, Reaction
-from util import assertDeepAlmostEqual
 
 
 class TestCRN(unittest.TestCase):
@@ -136,3 +137,57 @@ class TestCRN(unittest.TestCase):
         self.assertEqual((a, b), transitions[(c, d)])
 
 
+
+
+# taken from https://stackoverflow.com/questions/23549419/assert-that-two-dictionaries-are-almost-equal
+def assertDeepAlmostEqual(test_case: unittest.TestCase, expected: Any, actual: Any,
+                          *args: List, **kwargs: Dict) -> None:
+    """
+    Assert that two complex structures have almost equal contents.
+
+    Compares lists, dicts and tuples recursively. Checks numeric values
+    using test_case's :py:meth:`unittest.TestCase.assertAlmostEqual` and
+    checks all other values with :py:meth:`unittest.TestCase.assertEqual`.
+    Accepts additional positional and keyword arguments and pass those
+    intact to assertAlmostEqual() (that's how you specify comparison
+    precision).
+
+    Usage:
+
+    .. code-block:: python
+
+        from util import assertDeepAlmostEqual
+
+        assertDeepAlmostEqual(self, expected, actual)
+
+    Args:
+        test_case: TestCase object on which we can call all of the basic 'assert' methods.
+        expected: expected value
+        actual: actual value
+        *args: args to pass to TestCase.assertAlmostEqual
+        **kwargs: kwargs to pass to TestCase.assertAlmostEqual
+    """
+    is_root = not '__trace' in kwargs
+    trace = kwargs.pop('__trace', 'ROOT')
+    try:
+        if isinstance(expected, (int, float, numpy.long, complex)):
+            test_case.assertAlmostEqual(expected, actual, *args, **kwargs)
+        elif isinstance(expected, (list, tuple, numpy.ndarray)):
+            test_case.assertEqual(len(expected), len(actual))
+            for index in range(len(expected)):
+                v1, v2 = expected[index], actual[index]
+                assertDeepAlmostEqual(test_case, v1, v2,
+                                      __trace=repr(index), *args, **kwargs)
+        elif isinstance(expected, dict):
+            test_case.assertEqual(set(expected), set(actual))
+            for key in expected:
+                assertDeepAlmostEqual(test_case, expected[key], actual[key],
+                                      __trace=repr(key), *args, **kwargs)
+        else:
+            test_case.assertEqual(expected, actual)
+    except AssertionError as exc:
+        exc.__dict__.setdefault('traces', []).append(trace)
+        if is_root:
+            trace = ' -> '.join(reversed(exc.traces))
+            exc = AssertionError("%s\nTRACE: %s" % (exc.message, trace))
+        raise exc
